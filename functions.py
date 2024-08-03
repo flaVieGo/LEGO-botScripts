70d240506193610959eda854b278aec45db21b82
FlavioVieir <flaviovieira.gomes@outlook.com>
2024-08-03 07:42:27 -0300

    UPDATES

--- functions.py
+++ functions.py
@@ -6,45 +6,12 @@
 # func_linha[15] = VALOR         func_linha[16] = QTDE         func_linha[17] = OBS
 # func_linha[18] = LINK
 
-def whatsApp(func_linha, arquivo):
-   caminho = (fr'C:\Users\Flavio\_Flavio\LEGO\anuncios\{arquivo}.txt')            #casa
-   # caminho = (fr'C:\Users\r323741\Desktop\projetin\anuncios\{arquivo}.txt')     #trabalho
-   
-
-   with open(caminho, 'w') as texto:
-    texto.write(f'Descrição para WhatsApp\n\n')
-    texto.write(f'#{int(func_linha[1])}\n\n')
-
-    texto.write(f'Set: {func_linha[3]}\n')
-    texto.write(f'Linha: {func_linha[2]}\n')
-    texto.write(f'Condição: {func_linha[6]}\n')
-    texto.write(f'Peças: {int(func_linha[5])}\n')
-    texto.write(f'Ano: {int(func_linha[13])}\n')
-    if (func_linha[11]) == 'N/A':
-        texto.write(f'Minifiguras: N/A\n')
-    else:
-        texto.write(f'Minifiguras: {int(func_linha[11])}\n')
-    
-    if func_linha[6] == 'Usado':
-         texto.write(f'\nEmbalagem: {func_linha[7]} [obs]\n')
-         texto.write(f'Instruções: {func_linha[8]} [obs]\n')
-         if func_linha[9] == 'Completo':
-            texto.write(f'Peças: {func_linha[10]}\n')
-         else:
-            texto.write(f'Vide observações abaixo.\n')
-    
-    texto.write(f'\nPreço: R${func_linha[15]:.2f}\n')
-    texto.write(f'Qtde. Disponível: {int(func_linha[16])}\n\n')
-
-    texto.write(f'Link: {func_linha[18]}\n')
-
 
 def lojinha(func_linha, arquivo):
    caminho = (fr'C:\Users\Flavio\_Flavio\LEGO\anuncios\{arquivo}.txt')            #casa
    # caminho = (fr'C:\Users\r323741\Desktop\projetin\anuncios\{arquivo}.txt')     #trabalho
    
-     
-   with open(caminho, 'a') as texto:
+   with open(caminho, 'w') as texto:
       texto.write(f'\n\n\nDescrição para ML\n\n')
 
       texto.write(f'{int(func_linha[1])} LEGO®: {func_linha[2]} - {func_linha[3]}')
@@ -75,3 +42,35 @@ def lojinha(func_linha, arquivo):
       texto.write(f'Comercialização: {func_linha[14]}\n\n')
 
       texto.write(f'Obs.:\n')
+
+
+def whatsApp(func_linha, arquivo):
+   caminho = (fr'C:\Users\Flavio\_Flavio\LEGO\anuncios\{arquivo}.txt')            #casa
+   # caminho = (fr'C:\Users\r323741\Desktop\projetin\anuncios\{arquivo}.txt')     #trabalho
+   
+   with open(caminho, 'a') as texto:
+    texto.write(f'Descrição para WhatsApp\n\n')
+    texto.write(f'#{int(func_linha[1])}\n\n')
+
+    texto.write(f'Set: {func_linha[3]}\n')
+    texto.write(f'Linha: {func_linha[2]}\n')
+    texto.write(f'Condição: {func_linha[6]}\n')
+    texto.write(f'Peças: {int(func_linha[5])}\n')
+    texto.write(f'Ano: {int(func_linha[13])}\n')
+    if (func_linha[11]) == 'N/A':
+        texto.write(f'Minifiguras: N/A\n')
+    else:
+        texto.write(f'Minifiguras: {int(func_linha[11])}\n')
+    
+    if func_linha[6] == 'Usado':
+         texto.write(f'\nEmbalagem: {func_linha[7]} [obs]\n')
+         texto.write(f'Instruções: {func_linha[8]} [obs]\n')
+         if func_linha[9] == 'Completo':
+            texto.write(f'Peças: {func_linha[10]}\n')
+         else:
+            texto.write(f'Vide observações abaixo.\n')
+    
+    texto.write(f'\nPreço: R${func_linha[15]:.2f}\n')
+    texto.write(f'Qtde. Disponível: {int(func_linha[16])}\n\n')
+
+    texto.write(f'Link: {func_linha[18]}\n')
